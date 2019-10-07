5d730b2b72290705a65f911d83ae2e9bd65940e5
Vladislav Nikolov <vladis890@gmail.com>
2019-10-07 10:28:27 +0200

    Añade funcionalidad para recuperar una palabra en concreto #11

--- src/VocManager.js
+++ src/VocManager.js
@@ -11,6 +11,20 @@ class VocManager {
 		// Store it as a JSON object
 		fs.writeFileSync("out-test/out-example.json", JSON.stringify(voc));
 	}
+
+	getWordByType(word, type) {
+		// Read a word and store it in an object
+		var readObj = JSON.parse(fs.readFileSync("out-test/out-example.json"));
+
+		// THIS PART WILL CHANGE WHEN A DATABASE IS USED
+		// Find the element by the word and type given
+		console.log(readObj);
+		var vocabulary = readObj.find(element =>
+			element.word == word && element.type == type
+		);
+
+		return vocabulary;
+	}
 }
 
 module.exports = VocManager;
