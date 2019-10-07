3d7446bfbf7c8778b7c3758d75201a375db6f50a
Vladislav Nikolov <vladis890@gmail.com>
2019-10-07 13:08:12 +0200

    Cambiados tipos en los tests y añadido test que comrpueba que no se inserta la palabra #12

--- src/test/VocManager.test.js
+++ src/test/VocManager.test.js
@@ -6,58 +6,73 @@ const manager = new VocManager()
 
 test("Guarda nueva palabra en la 'BD' y esperar que sea la misma", () => {
 	// Store a word
-	manager.storeWord("Prueba", "sustantivo", ["Intento de algo"]);
+	manager.storeWord("Prueba", "noun", ["Intento de algo"]);
 
 	// Read the word from the file
-	var obj = JSON.parse(fs.readFileSync("out-test/out-example.json"));
+	var vocabulary = JSON.parse(fs.readFileSync("out-test/out-example.json"));
 
 	// Check if it has the same value
-	expect(obj).toEqual({word: "Prueba", type: "sustantivo", description: ["Intento de algo"]});
+	expect(vocabulary).toEqual({word: "Prueba", type: "noun", description: ["Intento de algo"]});
+});
+
+test("Fallar al intentar guardar una palabra que no tiene un tipo correcto", () => {
+	// Clear file
+	fs.writeFileSync("out-test/out-example.json", "");
+
+	// Try to store a word with a wrong type
+	manager.storeWord("Prueba", "nombre", ["Intento de algo"]);
+
+	// Read the 'stored' file
+	var vocabulary = fs.readFile("out-test/out-example.json");
+
+	// Check that nothing has been stored
+	expect(vocabulary).toBe(undefined);
+
 });
 
 test("Recuperar una palabra especifica de la 'BD'", () => {
 	// Write a group of words in the 'database'
 	fs.writeFileSync("out-test/out-example.json", JSON.stringify([
-		{word: "Prueba", type: "sustantivo", description: ["Intento de algo"]},
-		{word: "Test", type: "sustantivo", description: ["Prueba de un componente"]}
+		{word: "Prueba", type: "noun", description: ["Intento de algo"]},
+		{word: "Test", type: "noun", description: ["Prueba de un componente"]}
 	]));
 
 	// Retrieve the word
-	var word = manager.getWordByType("Test", "sustantivo");
+	var word = manager.getWordByType("Test", "noun");
 
 	// Check if it has the same value
-	expect(word).toEqual(new Vocabulary("Test", "sustantivo", ["Prueba de un componente"]));
+	expect(word).toEqual(new Vocabulary("Test", "noun", ["Prueba de un componente"]));
 });
 
 test("Recuperar un grupo de palabras de la 'BD' que tengan el mismo tipo", () => {
 	// Write a bunch of words to the 'database
 	fs.writeFileSync("out-test/out-example.json", JSON.stringify([
-		{word: "Prueba", type: "sustantivo", description: ["Intento de algo"]},
-		{word: "Test", type: "sustantivo", description: ["Prueba de un componente"]},
-		{word: "Comer", type: "verbo", description: ["Ingerir alimentos para obtener nutrientes"]}
+		{word: "Prueba", type: "noun", description: ["Intento de algo"]},
+		{word: "Test", type: "noun", description: ["Prueba de un componente"]},
+		{word: "Comer", type: "verb", description: ["Ingerir alimentos para obtener nutrientes"]}
 	]));
 
 	// Retrieve a group of words
-	var vocabulary = manager.getWordsSameType("sustantivo");
+	var vocabulary = manager.getWordsSameType("noun");
 
 	// Check if the response is the expected
 	expect(vocabulary.sort()).toEqual([
-		{word: "Prueba", type: "sustantivo", description: ["Intento de algo"]},
-		{word: "Test", type: "sustantivo", description: ["Prueba de un componente"]}
+		{word: "Prueba", type: "noun", description: ["Intento de algo"]},
+		{word: "Test", type: "noun", description: ["Prueba de un componente"]}
 	].sort());
 });
 
 test("Recuperar una palabra de la 'BD' cuando se busca que todas tengan el mismo tipo", () => {
 	// Write a bunch of words to the 'database
 	fs.writeFileSync("out-test/out-example.json", JSON.stringify([
-		{word: "Prueba", type: "sustantivo", description: ["Intento de algo"]},
-		{word: "Test", type: "sustantivo", description: ["Prueba de un componente"]},
-		{word: "Comer", type: "verbo", description: ["Ingerir alimentos para obtener nutrientes"]}
+		{word: "Prueba", type: "noun", description: ["Intento de algo"]},
+		{word: "Test", type: "noun", description: ["Prueba de un componente"]},
+		{word: "Comer", type: "verb", description: ["Ingerir alimentos para obtener nutrientes"]}
 	]));
 
 	// Retrieve a words of the specified type
-	var vocabulary = manager.getWordsSameType("verbo");
+	var vocabulary = manager.getWordsSameType("verb");
 
 	// Check if the response is the expected
-	expect(vocabulary).toEqual([{word: "Comer", type: "verbo", description: ["Ingerir alimentos para obtener nutrientes"]}]);
+	expect(vocabulary).toEqual([{word: "Comer", type: "verb", description: ["Ingerir alimentos para obtener nutrientes"]}]);
 });
