de5354aba43177eca64d85cb6acf4063af173a5f
Vladislav Nikolov <vladis890@gmail.com>
2019-10-07 11:06:30 +0200

    Añade tests para probar la obtencion de palabras por tipo #12

--- src/test/VocManager.test.js
+++ src/test/VocManager.test.js
@@ -26,5 +26,38 @@ test("Recuperar una palabra especifica de la 'BD'", () => {
 	var word = manager.getWordByType("Test", "sustantivo");
 
 	// Check if it has the same value
-	expect(word).toEqual(new Vocabulary("Test", "sustantivo", ["Prueba de un componente"]))
+	expect(word).toEqual(new Vocabulary("Test", "sustantivo", ["Prueba de un componente"]));
+});
+
+test("Recuperar un grupo de palabras de la 'BD' que tengan el mismo tipo", () => {
+	// Write a bunch of words to the 'database
+	fs.writeFileSync("out-test/out-example.json", JSON.stringify([
+		{word: "Prueba", type: "sustantivo", description: ["Intento de algo"]},
+		{word: "Test", type: "sustantivo", description: ["Prueba de un componente"]},
+		{word: "Comer", type: "verbo", description: ["Ingerir alimentos para obtener nutrientes"]}
+	]));
+
+	// Retrieve a group of words
+	var vocabulary = manager.getWordsSameType("sustantivo");
+
+	// Check if the response is the expected
+	expect(vocabulary.sort()).toEqual([
+		{word: "Prueba", type: "sustantivo", description: ["Intento de algo"]},
+		{word: "Test", type: "sustantivo", description: ["Prueba de un componente"]}
+	].sort());
+});
+
+test("Recuperar una palabra de la 'BD' cuando se busca que todas tengan el mismo tipo", () => {
+	// Write a bunch of words to the 'database
+	fs.writeFileSync("out-test/out-example.json", JSON.stringify([
+		{word: "Prueba", type: "sustantivo", description: ["Intento de algo"]},
+		{word: "Test", type: "sustantivo", description: ["Prueba de un componente"]},
+		{word: "Comer", type: "verbo", description: ["Ingerir alimentos para obtener nutrientes"]}
+	]));
+
+	// Retrieve a words of the specified type
+	var vocabulary = manager.getWordsSameType("verbo");
+
+	// Check if the response is the expected
+	expect(vocabulary).toEqual({word: "Comer", type: "verbo", description: ["Ingerir alimentos para obtener nutrientes"]});
 });
