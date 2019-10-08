c60bb28b838ba6ebb0514155de1ba05fa86e6ab3
Vladislav Nikolov <vladis890@gmail.com>
2019-10-08 23:59:51 +0200

    :bulb: Añade documentacion a la clase de gestion. Closes #11

--- src/VocManager.js
+++ src/VocManager.js
@@ -2,12 +2,25 @@ const Vocabulary = require("./Vocabulary");
 const fs = require("fs");
 
 class VocManager {
+	/**
+	 * Create a new Vocabulary Manager
+	 */
 	constructor() {}
 
+	/**
+	 * Get the allowed type of vocabulary
+	 * @return {Array} An Array of Strings
+	 */
 	static get ALLOWED_TYPES() {
 		return ["noun", "verb", "adverb", "adjective", "preposition", "conjunction", "expression", "determiner"]
 	}
 
+	/**
+	 * Store a new piece of vocabulary
+	 * @param {String} word - The word corresponding to the piece of vocabulary
+	 * @param {String} type - The type of the piece of vocabulary
+	 * @param {Array} desc - An array that describes the vocabulary
+	 */
 	storeWord(word, type, desc) {
 		// Check if the type is found in the allowed types
 		if (VocManager.ALLOWED_TYPES.indexOf(type) > -1) {
@@ -19,6 +32,12 @@ class VocManager {
 		}
 	}
 
+	/**
+	 * Get a piece of vocabulary identified by the word and its type
+	 * @param {String} word - The word corresponding to the piece of vocabulary
+	 * @param {String} type - The type of the piece of vocabulary
+	 * @return {Vocabulary} A Vocabulary object
+	 */
 	getVocabularyByType(word, type) {
 		// THIS PART WILL CHANGE WHEN A DATABASE IS USED
 		//*************************************************************************
@@ -38,6 +57,11 @@ class VocManager {
 		return vocabulary;
 	}
 
+	/**
+	 * Get a bunch of vocabulary with a specific type
+	 * @param {String} type - The type of the vocabulary to search
+	 * @return {Array} An Array of Vocabulary object
+	 */
 	getVocabularySameType(type) {
 		// THIS PART WILL CHANGE WHEN A DATABASE IS USED
 		//*************************************************************************
@@ -54,12 +78,24 @@ class VocManager {
 		return vocabulary;
 	}
 
+	/**
+	 * Modify the description of a piece of vocabulary identified by the word
+	 * and its type
+	 * @param {String} word - The word corresponding to the piece of vocabulary
+	 * @param {String} type - The type of the piece of vocabulary
+	 * @param {Array} newDesc - The new description which will be set to the piece of vocabulary
+	 */
 	modifyDescription(word, type, newDesc) {
 		if (VocManager.ALLOWED_TYPES.indexOf(type) > -1) {
 			// TODO
 		}
 	}
 
+	/**
+	 * Remove a piece of vocabulary from identified by the word and the type
+	 * @param {String} word - Word associated to the piece of vocabulary to be removed
+	 * @param {String} type - Type of the vocabulary to be removed
+	 */
 	deleteVocavulary(word, type){
 		if (VocManager.ALLOWED_TYPES.indexOf(type) > -1) {
 			// TODO
