7d709dbb8cd625779ad8fc723f90d134ad18b156
Vladislav Nikolov <vladis890@gmail.com>
2019-10-08 13:08:46 +0200

    Añade documentacion a estructura de datos de vocabulario. Closes #10

--- src/Vocabulary.js
+++ src/Vocabulary.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 
-// Definir la clase Vocabulary
+/* Class representing a piece of vocabulary */
 class Vocabulary {
+	/**
+	 * Create a piece of vocabulary
+	 * @param {String} word - The word to store
+	 * @param {String} type - The type of the word (noun, verb, etc.)
+	 * @param {Array} description - A description of the word (definitions, translations, etc.) 
+	 */
 	constructor(word, type, description) {
 		this.word = word;
 		this.type = type;
