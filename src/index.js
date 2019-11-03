4493b58182471c1543b82bd9bf287ffe85969dc5
Vladislav Nikolov <vladis890@gmail.com>
2019-11-03 14:09:44 +0100

    :heavy_plus_sign: Añade funcionalidad PUT #23

--- src/index.js
+++ src/index.js
@@ -2,29 +2,43 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var VocManager = require('./VocManager');
 
+var port = process.env.PORT || 8080;
 
 var app = express();
 const manager = new VocManager();
 
-
-var port = process.env.PORT || 8080;
-
-
 app.use(bodyParser.json());
+
+app.put('/:type/:word', (req, res) => {
+	var word = decodeURI(req.params.word);
+
+	try {
+		manager.addVocabulary(word, req.params.type, req.body.desc);
+	} catch (exception) {
+		res.status(400);
+	}
+
+	res.status(201).json(manager.getVocabularyWordType(word, req.params.type));
+});
 
 app.get('/', (req, res) => {
 	res.send('Hello world!').status(200);
-});
-
-app.get('/status', (req, res) => {
-	res.send({"status": "OK"});
 });
 
 app.get('/:type/:word', (req, res) => {
 	var wordParsed = decodeURI(req.params.word);
 	var vocablary = manager.getVocabularyByType(wordParsed, req.params.type);
 	res.send(vocablary).status(200);
-})
+});
+
+app.get(':/type', (req, res) => {
+
+});
+
+
+app.get('/status', (req, res) => {
+	res.send({"status": "OK"});
+});
 
 app.listen(port, () => {console.log('Server running at http://127.0.0.1:'+port+'/');})
 
