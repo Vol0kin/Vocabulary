f2042ca447aa54508912c8c0c7c5704a9a30ee41
Vladislav Nikolov <vladis890@gmail.com>
2019-11-03 02:27:22 +0100

    Añade coveralls a Gulp #34

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
-var jest = require('gulp-jest').default
+var jest = require('gulp-jest').default;
+var coveralls = require('gulp-coveralls');
 
 gulp.task('test-class', () => {
 	return gulp.src('test/VocManager.test.js').pipe(jest({
-		"collectCoverage": true
+		"collectCoverage": true,
+		"coverageDirectory": "test/coverage/"
 	}));
 });
+
+gulp.task('coveralls', () => {
+	return gulp.src('test/coverage/**/lcov.info').pipe(coveralls());
+})
