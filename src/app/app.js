860f5a72bdf666a9ef86d0541e3293397ca5df56
Vladislav Nikolov <vladis890@gmail.com>
2019-11-04 12:55:11 +0100

    Separa API de la parte del servidor. Closes #23

---
+++ src/app/app.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var VocManager = require('../VocManager');
+const ValueError = require('../exceptions/ValueError');
+
+var app = express();
+const manager = new VocManager();
+
+app.use(bodyParser.json());
+
+app.get('/status', (req, res) => {
+	res.send({"status": "OK"});
+});
+
+app.put('/:type/:word', (req, res) => {
+	var word = decodeURI(req.params.word);
+
+	try {
+		manager.addVocabulary(word, req.params.type, req.body.desc);
+		res.status(201).json(manager.getVocabularyWordType(word, req.params.type));
+	} catch (exception) {
+		if (exception instanceof TypeError) {
+			res.status(400).send({"error": "Invalid description! Expected array of strings!"});
+		} else {
+			res.status(400).send({"error": "Invalid type!"});
+		}
+	}
+});
+
+app.get('/:type/:word', (req, res) => {
+	var word = decodeURI(req.params.word);
+
+	try {
+		var vocabulary = manager.getVocabularyWordType(word, req.params.type);
+		res.status(200).json(vocabulary);
+	} catch (exception) {
+		if (exception instanceof ValueError) {
+			res.status(400).send({"error": "Invalid type!"});
+		} else {
+			res.status(404).send({"error": "Resource not found!"});
+		}
+	}
+});
+
+app.get('/:type', (req, res) => {
+	try {
+		var vocabulary = manager.getVocabularyType(req.params.type);
+		res.status(200).json(vocabulary);
+	} catch (exception) {
+		if (exception instanceof ValueError) {
+			res.status(400).send({"error": "Invalid type!"});
+		} else {
+			res.status(404).send({"error": "Resource not found!"});
+		}
+	}
+});
+
+app.post('/:type/:word', (req, res) => {
+	var word = decodeURI(req.params.word);
+
+	try {
+		manager.modifyDescription(word, req.params.type, req.body.desc);
+		res.status(200).json(manager.getVocabularyWordType(word, req.params.type));
+	} catch (exception) {
+		if (exception instanceof ValueError) {
+			res.status(400).send({"error": "Invalid type!"});
+		} else if (exception instanceof TypeError) {
+			res.status(400).send({"error": "Invalid description! Expected array of strings!"});
+		} else {
+			res.status(404).send({"error": "Resource not found!"});
+		}
+	}
+});
+
+app.delete('/:type/:word', (req, res) => {
+	var word = decodeURI(req.params.word);
+
+	try {
+		manager.deleteVocabulary(word, req.params.type);
+		res.sendStatus(204);
+	} catch (exception) {
+		if (exception instanceof ValueError) {
+			res.status(400).send({"error": "Invalid type!"});
+		} else {
+			res.status(404).send({"error": "Resource not found!"});
+		}
+	}
+});
+
+module.exports = app;
