e87787c2eaaf29399cc536ef1bf7d1384f1ec8fb
Vladislav Nikolov <vladis890@gmail.com>
2019-11-04 10:42:49 +0100

    Añade tests para metodos GET II y POST #24

--- test/index.test.js
+++ test/index.test.js
@@ -7,6 +7,10 @@ const comer = {
 	"description": ["ñam"]
 }
 
+const invalidDesc = {"error": "Invalid description! Expected array of strings!"};
+const invalidType = {"error": "Invalid type!"};
+const resourceNotFound = {"error": "Resource not found!"};
+
 describe("Testing REST API", () => {
 
 	describe("Testing PUT method", () => {
@@ -14,19 +18,21 @@ describe("Testing REST API", () => {
 		test("PUT /type/word non-valid type", () => {
 			return request(app).put("/verbo/comer").send({"desc": ["ñam"]}).then(response => {
 				expect(response.status).toBe(400);
+				expect(response.body).toEqual(invalidType);
 			});
 		});
 
 		test("PUT /type/word non-valid description", () => {
 			return request(app).put("/verb/comer").send({"desc": "ñam"}).then(response => {
 				expect(response.status).toBe(400);
+				expect(response.body).toEqual(invalidDesc);
 			});
 		});
 
 		test("PUT /type/word", () => {
 			return request(app).put("/verb/comer").send({"desc": ["ñam"]}).then(response  => {
 				expect(response.status).toBe(201);
-				expect(response.body).toEqual(comer);
+				expect(response.body).toStrictEqual(comer);
 			});
 		});
 	});
@@ -36,12 +42,14 @@ describe("Testing REST API", () => {
 		test("GET /type/word non-valid type", () => {
 			return request(app).get("/verbo/comer").then(response => {
 				expect(response.status).toBe(400);
+				expect(response.body).toEqual(invalidType);
 			});
 		});
 
 		test("GET /type/word non-valid word", () => {
 			return request(app).get("/verb/dormir").then(response => {
 				expect(response.status).toBe(404);
+				expect(response.body).toEqual(resourceNotFound);
 			});
 		});
 
@@ -51,5 +59,69 @@ describe("Testing REST API", () => {
 				expect(response.body).toEqual(comer);
 			});
 		});
+	});
+
+	describe("Testing GET method II", () => {
+
+		beforeAll(() => {
+			return request(app).put("/verb/dormir").send({"desc": ["zzz"]});
+		})
+		
+		test("GET /type non-valid type", () => {
+			return request(app).get("/vverb").then(response => {
+				expect(response.status).toBe(400);
+				expect(response.body).toEqual(invalidType);
+			});
+		});
+
+		test("GET /type no elements", () => {
+			return request(app).get("/noun").then(response => {
+				expect(response.status).toBe(404);
+				expect(response.body).toEqual(resourceNotFound);
+			});
+		});
+
+		test("GET /type", () => {
+			return request(app).get("/verb").then(response => {
+				expect(response.status).toBe(200);
+				expect(response.body).toBeInstanceOf(Array);
+				expect(response.body.length).toBe(2);
+			});
+		});
+	});
+
+	describe("Testing POST method", () => {
+
+		const desc = {
+			"desc": ["zzzzzz"]
+		}
+		
+		test("POST /type/word non-valid type", () => {
+			return request(app).post("/verbo/dormir").send(desc).then(response => {
+				expect(response.status).toBe(400);
+				expect(response.body).toEqual(invalidType);
+			});
+		});
+
+		test("POST /type/word non-valid word", () => {
+			return request(app).post("/verb/dormitar").send(desc).then(response => {
+				expect(response.status).toBe(404);
+				expect(response.body).toEqual(resourceNotFound);
+			});
+		});
+
+		test("POST /type/word non-valid description", () => {
+			return request(app).post("/verb/dormir").send({"desc": "zzzzzz"}).then(response => {
+				expect(response.status).toBe(400);
+				expect(response.body).toEqual(invalidDesc);
+			});
+		});
+
+		test("POST /type/word", () => {
+			return request(app).post("/verb/dormir").send(desc).then(response => {
+				expect(response.status).toBe(200);
+				expect(response.body).toStrictEqual({"word": "dormir", "type": "verb", "description": ["zzzzzz"]});
+			})
+		})
 	});
 });
