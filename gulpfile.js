c4d096dcb5b8efdb0cfcb0f1f5068adf82b912d4
Vladislav Nikolov <vladis890@gmail.com>
2019-11-05 02:29:11 +0100

    Añade pm2 a gulp #34

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,36 @@ var gulp = require('gulp');
 var jest = require('gulp-jest').default;
 var exec = require('child_process').exec;
 
+// Tarea para iniciar el gestor de proceos PM2
+gulp.task('start', function(cb) {
+	exec('pm2 start src/index.js -i 4',
+		function(err, stdout, stderr) {
+			console.log(stdout);
+			console.log(stderr);
+			cb(err);
+	});
+});
+
+// Tarea para parar el gestor de procesos PM2
+gulp.task('stop', function(cb) {
+	exec('pm2 stop all',
+		function(err, stdout, stderr) {
+			console.log(stdout);
+			console.log(stderr);
+			cb(err);
+	});
+});
+
+// Tarea para reiniciar el gestor de procesos PM2
+gulp.task('restart', function(cb) {
+	exec('pm2 restart src/index.js',
+		function(err, stdout, stderr) {
+			console.log(stdout);
+			console.log(stderr);
+			cb(err);
+	});
+});
+
 // Tarea para ejecutar los tests
 gulp.task('test', () => {
 	return gulp.src('test/*.test.js').pipe(jest({
