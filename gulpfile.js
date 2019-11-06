b9c6638214e95b212a39591fee19f416a2eaa9b7
Vladislav Nikolov <vladis890@gmail.com>
2019-11-06 01:33:01 +0100

    Añade capacidad de generar documentacion mediante tarea. Closes #34

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var jest = require('gulp-jest').default;
 var exec = require('child_process').exec;
+var apidoc = require('gulp-apidoc');
+var jsdoc = require('gulp-jsdoc3');
 
-// Tarea para iniciar el gestor de proceos PM2
+// Task to runs pm2
 gulp.task('start', function(cb) {
 	exec('pm2 start src/index.js -i 4',
 		function(err, stdout, stderr) {
@@ -12,7 +14,7 @@ gulp.task('start', function(cb) {
 	});
 });
 
-// Tarea para parar el gestor de procesos PM2
+// Task to stop pm2
 gulp.task('stop', function(cb) {
 	exec('pm2 stop all',
 		function(err, stdout, stderr) {
@@ -22,7 +24,7 @@ gulp.task('stop', function(cb) {
 	});
 });
 
-// Tarea para reiniciar el gestor de procesos PM2
+// Taskt to restart pm2
 gulp.task('restart', function(cb) {
 	exec('pm2 restart src/index.js',
 		function(err, stdout, stderr) {
@@ -32,7 +34,7 @@ gulp.task('restart', function(cb) {
 	});
 });
 
-// Tarea para ejecutar los tests
+// Task to run tests
 gulp.task('test', () => {
 	return gulp.src('test/*.test.js').pipe(jest({
 		"collectCoverage": true,
@@ -40,7 +42,7 @@ gulp.task('test', () => {
 	}));
 });
 
-// Tarea para obtener la cobertura del codigo
+// Task to get test coverage
 gulp.task('coveralls', function(cb) {
 	exec('cat test/coverage/lcov.info | node_modules/.bin/coveralls',
 		function(err, stdout, stderr) {
@@ -48,4 +50,18 @@ gulp.task('coveralls', function(cb) {
 			console.log(stderr);
 			cb(err);
 	});
+});
+
+// Task to generate documentation, both for classes and REST API
+gulp.task('doc', function(done) {
+	gulp.src(["src/Voc*.js", "src/exceptions/*.js", "src/Checker.js"], {read: false}).pipe(jsdoc({
+		"opts": {
+			"destination": "docs/Vocabulary"
+		}
+	}, done));
+
+	apidoc({
+		src: "src/app/",
+		dest: "docs/apirest/"
+	}, done);
 });
